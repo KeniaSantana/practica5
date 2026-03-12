543ec626ef2c9e4a59b5445288684f7f8d71d997
Kenia <23308060610371@cetis61.edu.mx>
2026-03-12 10:59:35 -0600

    x

---
+++ inicio/main.py
@@ -0,0 +1,34 @@
+
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Inicio de secion"
+    page.padding = 20
+
+    titulo = ft.Text(
+        "Inicia Sesion",
+        size=30,
+        weight=ft.FontWeight.BOLD,
+        text_align=ft.TextAlign.CENTER
+        
+    )
+    nombre=ft.TextField(
+    label="Nombre",
+    hint_text="Ingresa tu nombre",
+)
+    correo=ft.TextField(
+    label="Correo",
+    hint_text="Ingresa tu correo",
+)
+
+    password=ft.TextField(
+    label="Contraseña",
+    hint_text="Ingresa tu contraseña",
+    password=True,
+    can_reveal_password=True,
+
+)
+
+
+
+ft.app(main)
